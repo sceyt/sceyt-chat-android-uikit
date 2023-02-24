ee78dc131dd0ff617c8fa9613240d6bfb85aa3ba
Marat <hambikyan@gmail.com>
2023-02-24 20:07:21 +0400

    Added forgoten files.

--- SceytChatUiKit/src/main/java/com/sceyt/sceytchatuikit/presentation/uicomponents/messageinput/mention/MentionUserData.kt
+++ SceytChatUiKit/src/main/java/com/sceyt/sceytchatuikit/presentation/uicomponents/messageinput/mention/MentionUserData.kt
@@ -1,10 +1,18 @@
 package com.sceyt.sceytchatuikit.presentation.uicomponents.messageinput.mention
 
-data class MentionUserData(
-        val id: String,
-        val name: String) {
+import com.sceyt.sceytchatuikit.data.models.channels.SceytMember
+import com.sceyt.sceytchatuikit.extensions.getPresentableName
+import com.sceyt.sceytchatuikit.sceytconfigs.SceytKitConfig
 
-    override fun toString(): String {
-        return "@$name"
+data class MentionUserData(val member: SceytMember) {
+
+    val id get() = member.id
+
+    val user get() = member.user
+
+    fun getMentionedName(): String {
+        return "@${SceytKitConfig.userNameBuilder?.invoke(member.user) ?: member.getPresentableName()}"
     }
+
+    override fun toString() = getMentionedName()
 }
