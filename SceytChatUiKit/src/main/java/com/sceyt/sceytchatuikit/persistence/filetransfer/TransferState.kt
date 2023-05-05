865fc4321bb32fccdc27b85495d5cf4dc726407c
Marat <hambikyan@gmail.com>
2023-05-05 15:39:27 +0400

    Improved logic to update attachment transfer progress state.

--- SceytChatUiKit/src/main/java/com/sceyt/sceytchatuikit/persistence/filetransfer/TransferState.kt
+++ SceytChatUiKit/src/main/java/com/sceyt/sceytchatuikit/persistence/filetransfer/TransferState.kt
@@ -3,7 +3,18 @@ package com.sceyt.sceytchatuikit.persistence.filetransfer
 import androidx.core.view.isVisible
 import com.sceyt.sceytchatuikit.R
 import com.sceyt.sceytchatuikit.extensions.getCompatDrawable
-import com.sceyt.sceytchatuikit.persistence.filetransfer.TransferState.*
+import com.sceyt.sceytchatuikit.persistence.filetransfer.TransferState.Downloaded
+import com.sceyt.sceytchatuikit.persistence.filetransfer.TransferState.Downloading
+import com.sceyt.sceytchatuikit.persistence.filetransfer.TransferState.ErrorDownload
+import com.sceyt.sceytchatuikit.persistence.filetransfer.TransferState.ErrorUpload
+import com.sceyt.sceytchatuikit.persistence.filetransfer.TransferState.FilePathChanged
+import com.sceyt.sceytchatuikit.persistence.filetransfer.TransferState.PauseDownload
+import com.sceyt.sceytchatuikit.persistence.filetransfer.TransferState.PauseUpload
+import com.sceyt.sceytchatuikit.persistence.filetransfer.TransferState.PendingDownload
+import com.sceyt.sceytchatuikit.persistence.filetransfer.TransferState.PendingUpload
+import com.sceyt.sceytchatuikit.persistence.filetransfer.TransferState.ThumbLoaded
+import com.sceyt.sceytchatuikit.persistence.filetransfer.TransferState.Uploaded
+import com.sceyt.sceytchatuikit.persistence.filetransfer.TransferState.Uploading
 import com.sceyt.sceytchatuikit.presentation.customviews.SceytCircularProgressView
 
 enum class TransferState {
@@ -31,20 +42,27 @@ fun SceytCircularProgressView.getProgressWithState(state: TransferState, progres
             setIcon(context.getCompatDrawable(R.drawable.sceyt_ic_upload))
             isVisible = true
         }
+
         PendingDownload, ErrorDownload, PauseDownload -> {
             release()
             setTransferring(false)
             setIcon(context.getCompatDrawable(R.drawable.sceyt_ic_download))
             isVisible = true
         }
+
         Downloading, Uploading, FilePathChanged -> {
             setProgress(progressPercent)
             setIcon(context.getCompatDrawable(R.drawable.sceyt_ic_cancel_transfer))
             isVisible = true
         }
+
         Uploaded, Downloaded -> {
             isVisible = false
         }
-        ThumbLoaded -> return
+
+        ThumbLoaded -> {
+            if (progressPercent == 100f)
+                isVisible = false
+        }
     }
 }
