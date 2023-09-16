51e81b5f751bf4ef74b785c9e1477e7282d12550
Marat <hambikyan@gmail.com>
2023-09-16 17:08:28 +0400

    Removed user id in connect method.

--- SceytSimpleChatApp/src/main/java/com/sceyt/chat/simpleapp/presentation/MainActivity.kt
+++ SceytSimpleChatApp/src/main/java/com/sceyt/chat/simpleapp/presentation/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         }.root)
 
         // Step 1 - Connect Sceyt chat client
-        SceytKitClient.connect("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2OTQxNjg3NzksImV4cCI6MTY5NDI1NTE3OSwibmJmIjoxNjk0MTY4Nzc5LCJzdWIiOiJtIn0.W_UlmGIgQipA4MYr0Hm-6dZ8WuKZJWjMMzh4k36ATkgv1HamXFITfMYaSOJT_hp3RQi2UF3EAawcC-u5l35LJhRYmNopo_XUp7QZqQ76AhNXhz-OQBIsSkY3DGE6cr3xJ-TndOJKugHfXku0KcDfxCH8iMsUvxBo8kx2KFoYVFTTZUR7chy3EuBFp8XBHn_lZ6uCZJFoXoIJEz7H9zNV8SGeNCK_OnQh4xRrzATnTJUWhHST_iq9x5c6eXHhZFWco3a8LuvInlSqNSAbfRqr0aEAUQaT2ROJvddHU0snS0wTAc_g3-EJ_sCCblgO2dcp3oxrfEThJztiKNW21w-t2w", "testUser1")
+        SceytKitClient.connect("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2OTQxNjg3NzksImV4cCI6MTY5NDI1NTE3OSwibmJmIjoxNjk0MTY4Nzc5LCJzdWIiOiJtIn0.W_UlmGIgQipA4MYr0Hm-6dZ8WuKZJWjMMzh4k36ATkgv1HamXFITfMYaSOJT_hp3RQi2UF3EAawcC-u5l35LJhRYmNopo_XUp7QZqQ76AhNXhz-OQBIsSkY3DGE6cr3xJ-TndOJKugHfXku0KcDfxCH8iMsUvxBo8kx2KFoYVFTTZUR7chy3EuBFp8XBHn_lZ6uCZJFoXoIJEz7H9zNV8SGeNCK_OnQh4xRrzATnTJUWhHST_iq9x5c6eXHhZFWco3a8LuvInlSqNSAbfRqr0aEAUQaT2ROJvddHU0snS0wTAc_g3-EJ_sCCblgO2dcp3oxrfEThJztiKNW21w-t2w")
 
         // Step 2 - Connect the ChannelsViewModel to the ChannelsListView
         channelsViewModel.bind(binding.channelsListView, this)
