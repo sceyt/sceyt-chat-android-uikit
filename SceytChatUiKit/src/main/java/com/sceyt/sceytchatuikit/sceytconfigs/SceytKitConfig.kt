c8d1bf1474e4a3aae2287e6c70d99716259312dc
Marat <hambikyan@gmail.com>
2023-12-19 21:10:37 +0400

    Update avatar default colors.

--- SceytChatUiKit/src/main/java/com/sceyt/sceytchatuikit/sceytconfigs/SceytKitConfig.kt
+++ SceytChatUiKit/src/main/java/com/sceyt/sceytchatuikit/sceytconfigs/SceytKitConfig.kt
@@ -48,7 +48,7 @@ object SceytKitConfig {
 
     var defaultReactions = arrayListOf("\uD83D\uDE0E", "\uD83D\uDE02", "\uD83D\uDC4C", "\uD83D\uDE0D", "\uD83D\uDC4D", "\uD83D\uDE0F")
 
-    var avatarColors = arrayOf("#FF3E74", "#4F6AFF", "#FBB019", "#00CC99", "#9F35E7", "#63AFFF")
+    var avatarColors = arrayOf("#4F6AFF")
 
     var messageTransformer: MessageTransformer? = null
 
