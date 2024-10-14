9cc3d076a7b1b0229505f74c787a2dc071a97637
maratsct <marat@sceyt.com>
2024-10-14 12:23:42 +0400

    Fix issue to show message body attributes.

--- SceytChatUiKit/src/main/java/com/sceyt/chatuikit/persistence/mappers/BodyAttributeMapper.kt
+++ SceytChatUiKit/src/main/java/com/sceyt/chatuikit/persistence/mappers/BodyAttributeMapper.kt
@@ -1,13 +1,13 @@
 package com.sceyt.chatuikit.persistence.mappers
 
 import com.sceyt.chat.models.message.BodyAttribute
-import com.sceyt.chatuikit.presentation.components.channel.input.mention.Mention
 import com.sceyt.chatuikit.presentation.components.channel.input.format.BodyAttributeType
 import com.sceyt.chatuikit.presentation.components.channel.input.format.BodyStyleRange
 import com.sceyt.chatuikit.presentation.components.channel.input.format.toStyleType
+import com.sceyt.chatuikit.presentation.components.channel.input.mention.Mention
 
 fun BodyStyleRange.toBodyAttribute(): BodyAttribute {
-    return BodyAttribute(style.toString(), offset, length, null)
+    return BodyAttribute(style.value, offset, length, null)
 }
 
 fun BodyAttribute.toBodyStyleRange(): BodyStyleRange? {
