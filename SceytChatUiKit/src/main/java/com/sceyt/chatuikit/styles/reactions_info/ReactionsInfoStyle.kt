c6cf2826e8941ba481c9707c1e4ff749363d770e
maratsct <marat@sceyt.com>
2024-10-18 12:00:07 +0400

    Missing glass.

---
+++ SceytChatUiKit/src/main/java/com/sceyt/chatuikit/styles/reactions_info/ReactionsInfoStyle.kt
@@ -0,0 +1,54 @@
+package com.sceyt.chatuikit.styles.reactions_info
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.annotation.ColorInt
+import androidx.core.content.res.use
+import com.sceyt.chatuikit.R
+import com.sceyt.chatuikit.extensions.getCompatColor
+import com.sceyt.chatuikit.presentation.components.channel.messages.fragments.ReactionsInfoBottomSheetFragment
+import com.sceyt.chatuikit.styles.StyleCustomizer
+import com.sceyt.chatuikit.styles.common.BackgroundStyle
+import com.sceyt.chatuikit.styles.extensions.reaction_info.buildBackgroundStyle
+import com.sceyt.chatuikit.styles.extensions.reaction_info.buildHeaderBackgroundStyle
+import com.sceyt.chatuikit.theme.Colors
+import com.sceyt.chatuikit.theme.SceytChatUIKitTheme
+
+/**
+ * Style for [ReactionsInfoBottomSheetFragment].
+ * @property dividerColor Color of the divider between the header and the list of users, default is [Colors.borderColor].
+ * @property backgroundStyle Style for the background of the view, default is [buildBackgroundStyle].
+ * @property headerBackgroundStyle Style for the background of the header, default is [buildHeaderBackgroundStyle].
+ * @property headerItemStyle Style for the header item.
+ * @property reactedUserListStyle Style for the list of users who reacted to a message.
+ * */
+data class ReactionsInfoStyle(
+        @ColorInt val dividerColor: Int,
+        val backgroundStyle: BackgroundStyle,
+        val headerBackgroundStyle: BackgroundStyle,
+        val headerItemStyle: ReactionsInfoHeaderItemStyle,
+        val reactedUserListStyle: ReactedUserListStyle
+) {
+    companion object {
+        var styleCustomizer = StyleCustomizer<ReactionsInfoStyle> { _, style -> style }
+    }
+
+    internal class Builder(
+            internal val context: Context,
+            private val attributeSet: AttributeSet?
+    ) {
+        fun build(): ReactionsInfoStyle {
+            context.obtainStyledAttributes(attributeSet, R.styleable.ReactionInfo).use { array ->
+                val dividerColor = context.getCompatColor(SceytChatUIKitTheme.colors.borderColor)
+
+                return ReactionsInfoStyle(
+                    dividerColor = dividerColor,
+                    backgroundStyle = buildBackgroundStyle(array),
+                    headerBackgroundStyle = buildHeaderBackgroundStyle(array),
+                    headerItemStyle = ReactionsInfoHeaderItemStyle.Builder(context, attributeSet).build(),
+                    reactedUserListStyle = ReactedUserListStyle.Builder(context, attributeSet).build()
+                ).let { styleCustomizer.apply(context, it) }
+            }
+        }
+    }
+}
