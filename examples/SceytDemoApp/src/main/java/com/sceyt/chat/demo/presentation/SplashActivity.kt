8ba42c8e548a8603c9724ed7ab73205196fae21b
Marat Hambikyan <marat@sceyt.com>
2024-12-03 18:35:55 +0400

    Add splash.

---
+++ examples/SceytDemoApp/src/main/java/com/sceyt/chat/demo/presentation/SplashActivity.kt
@@ -0,0 +1,37 @@
+package com.sceyt.chat.demo.presentation
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.sceyt.chat.demo.R
+import com.sceyt.chat.demo.presentation.main.MainActivity
+import com.sceyt.chatuikit.extensions.launchActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        lifecycleScope.launch {
+            delay(1000)
+            launchActivity<MainActivity>(
+                enterAnimResId = R.anim.fade_in,
+                exitAnimResId = R.anim.fade_out
+            )
+            finishAfterTransition()
+        }
+    }
+}
