a82b0acc2cb10d866c9b6017f9dc1d31997b3b33
maratsct <marat@sceyt.com>
2025-01-10 14:39:06 +0400

    Add default value to params in searchChannelsWithUserIds method.

--- SceytChatUiKit/src/main/java/com/sceyt/chatuikit/persistence/interactor/ChannelInteractor.kt
+++ SceytChatUiKit/src/main/java/com/sceyt/chatuikit/persistence/interactor/ChannelInteractor.kt
@@ -12,8 +12,8 @@ import com.sceyt.chatuikit.data.models.channels.EditChannelData
 import com.sceyt.chatuikit.data.models.channels.GetAllChannelsResponse
 import com.sceyt.chatuikit.data.models.channels.SceytChannel
 import com.sceyt.chatuikit.data.models.messages.SceytMessage
-import com.sceyt.chatuikit.presentation.components.channel.input.mention.Mention
 import com.sceyt.chatuikit.presentation.components.channel.input.format.BodyStyleRange
+import com.sceyt.chatuikit.presentation.components.channel.input.mention.Mention
 import kotlinx.coroutines.flow.Flow
 
 interface ChannelInteractor {
@@ -30,7 +30,7 @@ interface ChannelInteractor {
             searchQuery: String,
             userIds: List<String>,
             config: ChannelListConfig,
-            params: SearchChannelParams,
+            params: SearchChannelParams = SearchChannelParams.default,
             includeSearchByUserDisplayName: Boolean,
             onlyMine: Boolean,
             ignoreDb: Boolean,
