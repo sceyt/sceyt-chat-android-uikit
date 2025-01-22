5fb5fa1436a5e43d17cff576427201ec7f5e0a1b
maratsct <marat@sceyt.com>
2025-01-22 16:14:29 +0400

    Remove HuaweiPushServiceProvider.kt

--- SceytChatUiKit/src/main/java/com/sceyt/chatuikit/push/PushValidator.kt
+++ SceytChatUiKit/src/main/java/com/sceyt/chatuikit/push/PushValidator.kt
@@ -1,11 +1,14 @@
 package com.sceyt.chatuikit.push
 
 object PushValidator {
-    private const val APP_VALUE = "vt_chat"
+    // Keys
     private const val KEY_APP = "app"
     private const val KEY_USER = "user"
     private const val KEY_CHANNEL = "channel"
     private const val KEY_MESSAGE = "message"
+
+    // Values
+    private const val APP_VALUE = "vt_chat"
 
     fun isSceytPushNotification(data: Map<String, String>): Boolean {
         return data[KEY_APP] == APP_VALUE
