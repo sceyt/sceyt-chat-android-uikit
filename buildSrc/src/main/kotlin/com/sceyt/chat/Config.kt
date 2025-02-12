0725864a51a73b247ba1c4fc52a2275edbaea17d
maratsct <marat@sceyt.com>
2025-02-12 19:15:43 +0400

    Update Version to 1.7.59023-SNAPSHOT

--- buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
+++ buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
@@ -10,7 +10,8 @@ object Config {
     const val mavenCentralGroup = "com.sceyt"
     const val mavenCentralArtifactId = "sceyt-chat-android-uikit"
     //const val mavenCentralVersion = "1.7.7"
-    const val mavenCentralVersion = "1.7.59022-SNAPSHOT"
+    //const val mavenCentralVersion = "local"
+    const val mavenCentralVersion = "1.7.59023-SNAPSHOT"
 
 
     /** App version */
