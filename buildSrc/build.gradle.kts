dd2076c5f8f4544fa2913ae6c5983cf140320b97
maratsct <marat@sceyt.com>
2025-02-13 12:51:07 +0400

    Set jvmTarget to 17 in SceytChatUiKit and remove it from buildSrc.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     `kotlin-dsl`
 }
@@ -12,9 +10,4 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
     implementation("com.android.tools.build:gradle:8.3.2")
-}
-
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "17"
 }
