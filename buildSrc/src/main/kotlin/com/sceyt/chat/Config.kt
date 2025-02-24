5faac2b0f8c0e2bb3e5e99ad25ef8b097053269a
Marat Hambikyan <marat@sceyt.com>
2025-02-24 14:14:27 +0400

    Version "1.7.59025-SNAPSHOT"

--- buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
+++ buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
@@ -9,9 +9,9 @@ object Config {
     /** MavenCentral */
     const val mavenCentralGroup = "com.sceyt"
     const val mavenCentralArtifactId = "sceyt-chat-android-uikit"
-    const val mavenCentralVersion = "1.7.8"
+    //const val mavenCentralVersion = "1.7.8"
     //const val mavenCentralVersion = "local"
-    //const val mavenCentralVersion = "1.7.59024-SNAPSHOT"
+    const val mavenCentralVersion = "1.7.59025-SNAPSHOT"
 
 
     /** App version */
