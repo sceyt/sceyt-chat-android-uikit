8fcf7368cec159dbb64d17d413969c1b2006f3ff
Marat Hambikyan <marat@sceyt.com>
2025-03-05 14:26:27 +0400

    Revert kotlin version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,6 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
     implementation("com.android.tools.build:gradle:8.3.2")
 }
