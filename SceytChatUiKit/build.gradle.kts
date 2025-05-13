b4de1ebf441393d34e4bf17152e918b71cef29a4
maratsct <marat@sceyt.com>
2025-05-13 15:02:50 +0400

    - Gradle update. - Version "1.8.12-SNAPSHOT"

--- SceytChatUiKit/build.gradle.kts
+++ SceytChatUiKit/build.gradle.kts
@@ -31,6 +31,10 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
+    
+    publishing {
+        singleVariant("release") {}
+    }
 }
 
 dependencies {
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,6 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
-    implementation("com.android.tools.build:gradle:8.3.2")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
+    implementation("com.android.tools.build:gradle:8.6.1")
 }
