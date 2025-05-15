ed442a45d12aa6b7ad4292e6c15ec81d38247329
maratsct <marat@sceyt.com>
2025-05-15 17:44:45 +0400

    - Improve TestLogger.kt.

--- SceytChatUiKit/src/test/java/com/sceyt/chatuikit/TestLogger.kt
+++ SceytChatUiKit/src/test/java/com/sceyt/chatuikit/TestLogger.kt
@@ -3,9 +3,11 @@ package com.sceyt.chatuikit
 import com.sceyt.chatuikit.logger.Priority
 import com.sceyt.chatuikit.logger.SceytLogger
 
-// Test logger implementation that prints log events
+/**
+ * A simple test logger for unit tests
+ */
 class TestLogger : SceytLogger {
     override fun log(priority: Priority, tag: String?, message: String?, throwable: Throwable?) {
-        println("[$priority] $tag: $message")
+        println("[$priority] $tag: $message ${throwable?.message ?: ""}")
     }
 }
