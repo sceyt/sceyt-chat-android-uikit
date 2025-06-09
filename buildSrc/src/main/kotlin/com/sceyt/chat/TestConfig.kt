105654a8d7e4a7b0c17818e07bea13bc6acb7b7d
maratsct <marat@sceyt.com>
2025-06-09 18:34:39 +0400

    - Fix TestConfig.kt warnings.

--- buildSrc/src/main/kotlin/com/sceyt/chat/TestConfig.kt
+++ buildSrc/src/main/kotlin/com/sceyt/chat/TestConfig.kt
@@ -7,19 +7,19 @@ import org.gradle.kotlin.dsl.withType
 
 fun Project.configureMockitoAgent(): Configuration {
     val mockitoAgent = configurations.create("mockitoAgent")
-    
+
     tasks.withType<Test>().configureEach {
         // Find the specific byte-buddy-agent JAR from the configuration
         val byteBuddyAgentJar = mockitoAgent.files.find { it.name.startsWith("byte-buddy-agent") }
 
         // Ensure the JAR was found before adding the jvmArgs
         if (byteBuddyAgentJar != null) {
-            jvmArgs = (jvmArgs ?: emptyList()) + "-javaagent:${byteBuddyAgentJar.absolutePath}"
+            jvmArgs = jvmArgs.orEmpty().plus("-javaagent:${byteBuddyAgentJar.absolutePath}")
         } else {
             // Log a warning or error if the byte-buddy-agent JAR is not found
             logger.warn("Byte-buddy-agent JAR not found in the 'mockitoAgent' configuration.")
         }
     }
-    
+
     return mockitoAgent
 } 
