4b418b6909a206086056a1e5058cbd62abcc4c83
maratsct <marat@sceyt.com>
2025-08-18 17:11:09 +0400

    - Update ChatSdk and gradle dependencies. - Version "1.9.211-SNAPSHOT"

--- buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
+++ buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
@@ -12,7 +12,7 @@ object Config {
 
     // const val mavenCentralVersion = "1.9.2"
     // const val mavenCentralVersion = "local"
-    const val mavenCentralVersion = "1.9.210-SNAPSHOT"
+    const val mavenCentralVersion = "1.9.211-SNAPSHOT"
 
 
     /** App version */
