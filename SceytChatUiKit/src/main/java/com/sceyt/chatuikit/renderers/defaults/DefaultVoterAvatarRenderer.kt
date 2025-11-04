c10f505cf59c71ddbfb595f10599e2faeb83ded7
maratsct <marat@sceyt.com>
2025-11-04 13:07:57 +0400

    - Set placeholder and error placeholder for voter avatars. - Use the default avatar for placeholder and error states.

--- SceytChatUiKit/src/main/java/com/sceyt/chatuikit/renderers/defaults/DefaultVoterAvatarRenderer.kt
+++ SceytChatUiKit/src/main/java/com/sceyt/chatuikit/renderers/defaults/DefaultVoterAvatarRenderer.kt
@@ -10,16 +10,19 @@ import com.sceyt.chatuikit.styles.messages_list.item.VoterAvatarRendererAttribut
 open class DefaultVoterAvatarRenderer : AvatarRenderer<VoterAvatarRendererAttributes> {
 
     override fun render(
-            context: Context,
-            from: VoterAvatarRendererAttributes,
-            style: AvatarStyle,
-            avatarView: AvatarView,
+        context: Context,
+        from: VoterAvatarRendererAttributes,
+        style: AvatarStyle,
+        avatarView: AvatarView,
     ) {
-        val defaultAvatar = SceytChatUIKit.providers.userDefaultAvatarProvider.provide(context, from.voter)
+        val defaultAvatar =
+            SceytChatUIKit.providers.userDefaultAvatarProvider.provide(context, from.voter)
         avatarView.appearanceBuilder()
             .setStyle(style)
             .setDefaultAvatar(defaultAvatar)
             .setImageUrl(from.voter.avatarURL)
+            .setErrorPlaceholder(AvatarView.AvatarErrorPlaceHolder.FromDrawable(defaultAvatar))
+            .setPlaceholder(AvatarView.AvatarPlaceholder.FromDrawable(defaultAvatar))
             .setBorder(5f, from.bubbleBackgroundStyle.backgroundColor)
             .build()
             .applyToAvatar()
