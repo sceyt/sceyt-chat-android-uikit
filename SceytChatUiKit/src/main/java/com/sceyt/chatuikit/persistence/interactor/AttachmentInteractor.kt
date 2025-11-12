81de1341afd31f8756a29bc7149a8890c757d3e2
maratsct <marat@sceyt.com>
2025-11-12 19:25:53 +0400

    - Improvements

--- SceytChatUiKit/src/main/java/com/sceyt/chatuikit/persistence/interactor/AttachmentInteractor.kt
+++ SceytChatUiKit/src/main/java/com/sceyt/chatuikit/persistence/interactor/AttachmentInteractor.kt
@@ -11,19 +11,31 @@ import com.sceyt.chatuikit.persistence.file_transfer.TransferData
 import kotlinx.coroutines.flow.Flow
 
 interface AttachmentInteractor {
-    suspend fun getPrevAttachments(conversationId: Long, lastAttachmentId: Long, types: List<String>,
-                                   offset: Int, ignoreDb: Boolean = false, loadKeyData: LoadKeyData = LoadKeyData()): Flow<PaginationResponse<AttachmentWithUserData>>
+    suspend fun getPrevAttachments(
+        conversationId: Long, lastAttachmentId: Long, types: List<String>,
+        offset: Int, ignoreDb: Boolean = false, loadKeyData: LoadKeyData = LoadKeyData()
+    ): Flow<PaginationResponse<AttachmentWithUserData>>
 
-    suspend fun getNextAttachments(conversationId: Long, lastAttachmentId: Long, types: List<String>,
-                                   offset: Int, ignoreDb: Boolean = false, loadKeyData: LoadKeyData = LoadKeyData()): Flow<PaginationResponse<AttachmentWithUserData>>
+    suspend fun getNextAttachments(
+        conversationId: Long, lastAttachmentId: Long, types: List<String>,
+        offset: Int, ignoreDb: Boolean = false, loadKeyData: LoadKeyData = LoadKeyData()
+    ): Flow<PaginationResponse<AttachmentWithUserData>>
 
-    suspend fun getNearAttachments(conversationId: Long, attachmentId: Long, types: List<String>,
-                                   offset: Int, ignoreDb: Boolean = false, loadKeyData: LoadKeyData = LoadKeyData()): Flow<PaginationResponse<AttachmentWithUserData>>
+    suspend fun getNearAttachments(
+        conversationId: Long, attachmentId: Long, types: List<String>,
+        offset: Int, ignoreDb: Boolean = false, loadKeyData: LoadKeyData = LoadKeyData()
+    ): Flow<PaginationResponse<AttachmentWithUserData>>
 
     suspend fun updateAttachmentIdAndMessageId(message: SceytMessage)
     suspend fun updateTransferDataByMsgTid(data: TransferData)
     suspend fun updateAttachmentWithTransferData(data: TransferData)
-    suspend fun updateAttachmentFilePathAndMetadata(messageTid: Long, newPath: String, fileSize: Long, metadata: String?)
+    suspend fun updateAttachmentFilePathAndMetadata(
+        messageTid: Long,
+        newPath: String,
+        fileSize: Long,
+        metadata: String?
+    )
+
     suspend fun getFileChecksumData(filePath: String?): FileChecksumData?
     suspend fun getLinkPreviewData(link: String?): SceytResponse<LinkPreviewDetails>
     suspend fun upsertLinkPreviewData(linkDetails: LinkPreviewDetails)
