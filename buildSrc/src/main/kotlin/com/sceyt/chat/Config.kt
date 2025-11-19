58bee4889874f7eeb105ee1b480157f564a5b2d2
maratsct <marat@sceyt.com>
2025-11-19 15:18:42 +0400

    - Revert Koin. - Version `2.0.502-SNAPSHOT`.

--- buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
+++ buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
@@ -12,7 +12,7 @@ object Config {
 
     //const val mavenCentralVersion = "2.0.5"
     //const val mavenCentralVersion = "local"
-    const val mavenCentralVersion = "2.0.501-SNAPSHOT"
+    const val mavenCentralVersion = "2.0.502-SNAPSHOT"
 
 
     /** App version */
