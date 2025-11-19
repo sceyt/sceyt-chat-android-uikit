e6bd80e26c5df814ef5a7fabe8134cb50a3a17fd
maratsct <marat@sceyt.com>
2025-11-19 12:50:22 +0400

    - Update Koin to version. - Version `2.0.501-SNAPSHOT`.

--- buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
+++ buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
@@ -10,9 +10,9 @@ object Config {
     const val mavenCentralGroup = "com.sceyt"
     const val mavenCentralArtifactId = "sceyt-chat-android-uikit"
 
-    const val mavenCentralVersion = "2.0.5"
+    //const val mavenCentralVersion = "2.0.5"
     //const val mavenCentralVersion = "local"
-    //const val mavenCentralVersion = "2.0.305-SNAPSHOT"
+    const val mavenCentralVersion = "2.0.501-SNAPSHOT"
 
 
     /** App version */
