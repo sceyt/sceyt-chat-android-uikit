100972c365087a56d82cf6fbae1bed558383a231
maratsct <marat@sceyt.com>
2025-12-02 19:04:03 +0400

    - Add `SerializedName` annotations to system message metadata. - Version "2.0.512-SNAPSHOT"

--- SceytChatUiKit/src/main/java/com/sceyt/chatuikit/data/models/messages/SystemMessageMetadata.kt
+++ SceytChatUiKit/src/main/java/com/sceyt/chatuikit/data/models/messages/SystemMessageMetadata.kt
@@ -1,9 +1,12 @@
 package com.sceyt.chatuikit.data.models.messages
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * Metadata for member added/removed system messages
  */
 data class MembersMetaData(
+    @SerializedName("m")
     val members: List<String>?
 )
 
@@ -11,5 +14,6 @@ data class MembersMetaData(
  * Metadata for disappearing messages system message
  */
 data class DisappearingMessageMetadata(
+    @SerializedName("autoDeletePeriod")
     val duration: String?
 )
--- buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
+++ buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
@@ -12,7 +12,7 @@ object Config {
 
     //const val mavenCentralVersion = "2.0.5"
     //const val mavenCentralVersion = "local"
-    const val mavenCentralVersion = "2.0.511-SNAPSHOT"
+    const val mavenCentralVersion = "2.0.512-SNAPSHOT"
 
 
     /** App version */
