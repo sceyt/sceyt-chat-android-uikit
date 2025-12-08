130093cb977f6b6f8c09bd73ebbaa2b311b1be42
maratsct <marat@sceyt.com>
2025-12-08 14:56:56 +0400

    - Add transaction support to getAllPendingVotesDb query in PendingPollVoteDao - Version "2.0.519-SNAPSHOT"

--- SceytChatUiKit/src/main/java/com/sceyt/chatuikit/persistence/database/dao/PendingPollVoteDao.kt
+++ SceytChatUiKit/src/main/java/com/sceyt/chatuikit/persistence/database/dao/PendingPollVoteDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import com.sceyt.chatuikit.persistence.database.DatabaseConstants.PENDING_POLL_VOTE_TABLE
 import com.sceyt.chatuikit.persistence.database.entity.messages.PendingPollVoteDb
 import com.sceyt.chatuikit.persistence.database.entity.pendings.PendingPollVoteEntity
@@ -26,6 +27,7 @@ internal abstract class PendingPollVoteDao {
     @Query("DELETE FROM $PENDING_POLL_VOTE_TABLE WHERE messageTid =:messageTid and pollId = :pollId")
     abstract suspend fun deletePendingVotesByPollId(messageTid: Long, pollId: String): Int
 
+    @Transaction
     @Query("SELECT * FROM $PENDING_POLL_VOTE_TABLE ORDER BY createdAt ASC")
     abstract suspend fun getAllPendingVotesDb(): List<PendingPollVoteDb>
 
