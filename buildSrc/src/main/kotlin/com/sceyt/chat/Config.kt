0ca6f3fb8f79dba0e87491c394c252a9b414b3ca
maratsct <marat@sceyt.com>
2025-12-15 17:20:46 +0400

    Update chat sdk. Version 2.0.524-SNAPSHOT

--- buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
+++ buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
@@ -12,7 +12,7 @@ object Config {
 
     //const val mavenCentralVersion = "2.0.5"
     //const val mavenCentralVersion = "local"
-    const val mavenCentralVersion = "2.0.523-SNAPSHOT"
+    const val mavenCentralVersion = "2.0.524-SNAPSHOT"
 
 
     /** App version */
