7f44f09623f6c82831f6bf7efb644c79acddec43
maratsct <marat@sceyt.com>
2026-01-23 12:55:37 +0400

    - Update ChatSDK. - Version "2.0.7"

--- buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
+++ buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
@@ -10,9 +10,9 @@ object Config {
     const val mavenCentralGroup = "com.sceyt"
     const val mavenCentralArtifactId = "sceyt-chat-android-uikit"
 
-    // const val mavenCentralVersion = "2.0.6"
+    const val mavenCentralVersion = "2.0.7"
     //const val mavenCentralVersion = "local"
-    const val mavenCentralVersion = "2.0.611-SNAPSHOT"
+    // const val mavenCentralVersion = "2.0.612-SNAPSHOT"
 
     /** App version */
     const val versionCode = 48
