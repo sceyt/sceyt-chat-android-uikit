821878ac3bda7e505381b81f92873d9cef59e0bb
maratsct <marat@sceyt.com>
2026-03-12 11:55:26 +0400

    - Implement link preview loading in `ShareViewModel` to fetch and display metadata when sharing links or files with links. - Fix logic in `MessagesListHeaderView` to correctly allow copying messages that are not "view once" messages. - Version 2.0.807-SNAPSHOT.

--- SceytChatUiKit/src/main/java/com/sceyt/chatuikit/presentation/components/share/viewmodel/ShareViewModel.kt
+++ SceytChatUiKit/src/main/java/com/sceyt/chatuikit/presentation/components/share/viewmodel/ShareViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.net.Uri
 import android.util.Log
 import androidx.documentfile.provider.DocumentFile
+import androidx.lifecycle.viewModelScope
 import com.sceyt.chat.models.attachment.Attachment
 import com.sceyt.chat.models.message.Message.MessageBuilder
 import com.sceyt.chat.wrapper.ClientWrapper
@@ -16,24 +17,32 @@ import com.sceyt.chatuikit.extensions.copyFile
 import com.sceyt.chatuikit.extensions.extractLinks
 import com.sceyt.chatuikit.extensions.getFileSize
 import com.sceyt.chatuikit.koin.SceytKoinComponent
+import com.sceyt.chatuikit.persistence.extensions.safeResume
 import com.sceyt.chatuikit.persistence.interactor.MessageInteractor
 import com.sceyt.chatuikit.persistence.mappers.getAttachmentType
 import com.sceyt.chatuikit.persistence.mappers.toMetadata
+import com.sceyt.chatuikit.presentation.components.channel.input.link.SingleLinkDetailsProvider
 import com.sceyt.chatuikit.presentation.root.BaseViewModel
 import com.sceyt.chatuikit.shared.utils.FilePathUtil
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeoutOrNull
 import org.koin.core.component.inject
 import java.io.File
 import java.io.FileInputStream
 import java.util.concurrent.atomic.AtomicInteger
+import kotlin.time.Duration.Companion.seconds
 
 class ShareViewModel : BaseViewModel(), SceytKoinComponent {
     private val messageInteractor by inject<MessageInteractor>()
     private val application by inject<Application>()
+    private val linkDetailsProvider by lazy {
+        SingleLinkDetailsProvider(application, viewModelScope)
+    }
 
     fun sendTextMessage(vararg channelIds: Long, body: String) = callbackFlow {
         trySend(State.Loading)
@@ -43,6 +52,8 @@ class ShareViewModel : BaseViewModel(), SceytKoinComponent {
 
         val count = AtomicInteger(0)
         withContext(Dispatchers.IO) {
+            val linkPreviewDetails = if (isContainsLink) loadLinkPreview(links[0]) else null
+
             channelIds.forEach { channelId ->
                 val message = MessageBuilder(channelId)
                     .setBody(body)
@@ -51,6 +62,7 @@ class ShareViewModel : BaseViewModel(), SceytKoinComponent {
                     .apply {
                         if (isContainsLink) {
                             val link = links[0]
+                            val details = linkPreviewDetails ?: LinkPreviewDetails.hiddenLink(link)
                             setAttachments(
                                 arrayOf(
                                     buildAttachment(
@@ -59,7 +71,7 @@ class ShareViewModel : BaseViewModel(), SceytKoinComponent {
                                         typeEnum = AttachmentTypeEnum.Link,
                                         fileName = "",
                                         fileSize = 0,
-                                        metadata = LinkPreviewDetails.hiddenLink(link).toMetadata()
+                                        metadata = details.toMetadata()
                                     )
                                 )
                             )
@@ -83,56 +95,78 @@ class ShareViewModel : BaseViewModel(), SceytKoinComponent {
     }
 
 
-    fun sendFilesMessage(vararg channelIds: Long, uris: List<Uri>, messageBody: String) =
-        callbackFlow {
-            trySend(State.Loading)
-            val links = messageBody.extractLinks()
-            val isContainsLink = links.isNotEmpty()
+    fun sendFilesMessage(
+        vararg channelIds: Long,
+        uris: List<Uri>,
+        messageBody: String
+    ) = callbackFlow {
+        trySend(State.Loading)
+        val links = messageBody.extractLinks()
+        val isContainsLink = links.isNotEmpty()
 
-            withContext(Dispatchers.IO) {
-                val paths = getPathFromFile(*uris.toTypedArray()).toMutableList()
+        withContext(Dispatchers.IO) {
+            val paths = getPathFromFile(*uris.toTypedArray()).toMutableList()
+            val linkPreviewDetails = if (isContainsLink) loadLinkPreview(links[0]) else null
 
-                channelIds.forEach { channelId ->
-                    val attachments = paths.map { path ->
-                        val fileName = File(path).name
-                        buildAttachment(
-                            path = path,
-                            url = "",
-                            typeEnum = getAttachmentType(path),
-                            fileName = fileName,
-                            fileSize = getFileSize(path),
-                            metadata = ""
-                        )
-                    }
-                    attachments.mapIndexed { index, attachment ->
-                        val message = MessageBuilder(channelId)
-                            .setBody(if (index == 0) messageBody else "")
-                            .apply {
-                                if (index == 0 && isContainsLink) {
-                                    val linkUrl = links[0]
-                                    val link = buildAttachment(
-                                        path = "",
-                                        url = linkUrl,
-                                        typeEnum = AttachmentTypeEnum.Link,
-                                        fileName = "",
-                                        fileSize = 0,
-                                        metadata = LinkPreviewDetails.hiddenLink(linkUrl).toMetadata()
-                                    )
-                                    setAttachments(arrayOf(attachment, link))
-                                } else setAttachments(arrayOf(attachment))
-                            }
-                            .setTid(ClientWrapper.generateTid())
-                            .setType(SceytMessageType.Media.value)
-                            .build()
+            channelIds.forEach { channelId ->
+                val attachments = paths.map { path ->
+                    val fileName = File(path).name
+                    buildAttachment(
+                        path = path,
+                        url = "",
+                        typeEnum = getAttachmentType(path),
+                        fileName = fileName,
+                        fileSize = getFileSize(path),
+                        metadata = ""
+                    )
+                }
+                attachments.mapIndexed { index, attachment ->
+                    val message = MessageBuilder(channelId)
+                        .setBody(if (index == 0) messageBody else "")
+                        .apply {
+                            if (index == 0 && isContainsLink) {
+                                val linkUrl = links[0]
+                                val details =
+                                    linkPreviewDetails ?: LinkPreviewDetails.hiddenLink(linkUrl)
+                                val link = buildAttachment(
+                                    path = "",
+                                    url = linkUrl,
+                                    typeEnum = AttachmentTypeEnum.Link,
+                                    fileName = "",
+                                    fileSize = 0,
+                                    metadata = details.toMetadata()
+                                )
+                                setAttachments(arrayOf(attachment, link))
+                            } else setAttachments(arrayOf(attachment))
+                        }
+                        .setTid(ClientWrapper.generateTid())
+                        .setType(SceytMessageType.Media.value)
+                        .build()
 
-                        messageInteractor.sendSharedFileMessage(channelId, message)
-                    }
+                    messageInteractor.sendSharedFileMessage(channelId, message)
                 }
             }
-
-            trySend(State.Finish)
-            awaitClose()
         }
+
+        trySend(State.Finish)
+        awaitClose()
+    }
+
+    private suspend fun loadLinkPreview(link: String): LinkPreviewDetails? {
+        return withTimeoutOrNull(3.seconds) {
+            suspendCancellableCoroutine { cont ->
+                linkDetailsProvider.loadLinkDetails(
+                    text = link,
+                    detailsCallback = { details ->
+                        cont.safeResume(details)
+                    },
+                    imageSizeCallback = { },
+                    thumbCallback = { }
+                )
+                cont.invokeOnCancellation { linkDetailsProvider.cancel() }
+            }
+        }
+    }
 
     private fun buildAttachment(
         path: String,
--- buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
+++ buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
@@ -12,7 +12,7 @@ object Config {
 
     //const val mavenCentralVersion = "2.0.8"
     //const val mavenCentralVersion = "local"
-    const val mavenCentralVersion = "2.0.806-SNAPSHOT"
+    const val mavenCentralVersion = "2.0.807-SNAPSHOT"
 
     /** App version */
     const val versionCode = 52
