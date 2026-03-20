82ed4042e633bc80bde40444887d9305ba10ca89
maratsct <marat@sceyt.com>
2026-03-20 19:33:12 +0400

    - Version 1.2.3 (55)

--- buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
+++ buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
@@ -15,6 +15,6 @@ object Config {
     const val mavenCentralVersion = "2.0.903-SNAPSHOT"
 
     /** App version */
-    const val versionCode = 54
-    const val versionName = "1.2.2"
+    const val versionCode = 55
+    const val versionName = "1.2.3"
 }
