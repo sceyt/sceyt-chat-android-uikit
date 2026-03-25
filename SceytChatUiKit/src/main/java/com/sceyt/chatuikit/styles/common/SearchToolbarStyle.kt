d252a6c9e61ff8d21f2304184c6e22a88b5d5dbe
maratsct <marat@sceyt.com>
2026-03-25 15:36:31 +0400

    Enhance `SearchableToolbar` styling and customization options
    - Added new XML attributes for `SearchableToolbar` including background color, underline color, font families, and text styles for both title and search input.
    - Refactored `SearchableToolbar` initialization to utilize `SearchToolbarStyle` for consistent style application.
    - Updated `SearchToolbarStyle` to conditionally apply background and underline colors only when they are explicitly set.

--- SceytChatUiKit/src/main/java/com/sceyt/chatuikit/styles/common/SearchToolbarStyle.kt
+++ SceytChatUiKit/src/main/java/com/sceyt/chatuikit/styles/common/SearchToolbarStyle.kt
@@ -1,16 +1,21 @@
 package com.sceyt.chatuikit.styles.common
 
 import com.sceyt.chatuikit.presentation.custom_views.SearchableToolbar
+import com.sceyt.chatuikit.styles.StyleConstants.UNSET_COLOR
 
 data class SearchToolbarStyle(
-        val toolbarStyle: ToolbarStyle,
-        val searchInputStyle: SearchInputStyle
+    val toolbarStyle: ToolbarStyle,
+    val searchInputStyle: SearchInputStyle
 ) {
     fun apply(toolbar: SearchableToolbar) {
         with(toolbarStyle) {
-            toolbar.setBackgroundColor(backgroundColor)
+            if (backgroundColor != UNSET_COLOR)
+                toolbar.setBackgroundColor(backgroundColor)
+
+            if (underlineColor != UNSET_COLOR)
+                toolbar.setBorderColor(underlineColor)
+
             toolbar.setTitleTextStyle(titleTextStyle)
-            toolbar.setBorderColor(underlineColor)
             toolbar.setNavigationIcon(navigationIcon)
             toolbar.setSearchInputStyle(searchInputStyle)
         }
