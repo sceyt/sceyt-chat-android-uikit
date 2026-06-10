b33141d0094128915243dd2ac1d1a8410f6f69b3
maratsct <marat@sceyt.com>
2026-06-10 12:56:46 +0400

    - Version "2.1.508-SNAPSHOT"

--- buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
+++ buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
@@ -12,7 +12,7 @@ object Config {
 
     // const val mavenCentralVersion = "2.1.5"
     //const val mavenCentralVersion = "local"
-    const val mavenCentralVersion = "2.1.507-SNAPSHOT"
+    const val mavenCentralVersion = "2.1.508-SNAPSHOT"
 
     /** App version */
     const val versionCode = 59
