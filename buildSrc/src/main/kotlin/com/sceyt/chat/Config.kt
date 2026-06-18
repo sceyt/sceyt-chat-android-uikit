9cb20ad028ab1f3ce0c2963aa5b9f999af980d07
maratsct <marat@sceyt.com>
2026-06-18 13:01:35 +0400

    - Update Chat and Call SDKs. - Version "2.1.518-SNAPSHOT"

--- buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
+++ buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
@@ -12,7 +12,7 @@ object Config {
 
     // const val mavenCentralVersion = "2.1.5"
     // const val mavenCentralVersion = "local"
-    const val mavenCentralVersion = "2.1.517-SNAPSHOT"
+    const val mavenCentralVersion = "2.1.518-SNAPSHOT"
 
     /** App version */
     const val versionCode = 59
