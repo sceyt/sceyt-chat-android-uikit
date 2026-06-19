c7907475ad52eb0efcd65f5e9edb17c589ab97ac
maratsct <marat@sceyt.com>
2026-06-19 12:34:17 +0400

    - Downgraded Lifecycle (Compose, Runtime, ViewModel, Process) and WorkManager dependencies to version `2.10.0` in `libs.versions.toml`. - Version `2.1.520-SNAPSHOT`

--- buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
+++ buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
@@ -12,7 +12,7 @@ object Config {
 
     // const val mavenCentralVersion = "2.1.5"
     // const val mavenCentralVersion = "local"
-    const val mavenCentralVersion = "2.1.519-SNAPSHOT"
+    const val mavenCentralVersion = "2.1.520-SNAPSHOT"
 
     /** App version */
     const val versionCode = 59
