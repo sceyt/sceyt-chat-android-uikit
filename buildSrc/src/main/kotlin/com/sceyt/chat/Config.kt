f8fca0708e5a645b3619b5b542b367e40ffa78c6
Marat <38978831+hambikyan1995@users.noreply.github.com>
2026-07-17 12:18:05 +0400

    - Version "2.1.523002-SNAPSHOT"

--- buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
+++ buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
@@ -12,7 +12,7 @@ object Config {
 
     // const val mavenCentralVersion = "2.1.5"
     // const val mavenCentralVersion = "local"
-    const val mavenCentralVersion = "2.1.523001-SNAPSHOT"
+    const val mavenCentralVersion = "2.1.523002-SNAPSHOT"
 
     /** App version */
     const val versionCode = 59
