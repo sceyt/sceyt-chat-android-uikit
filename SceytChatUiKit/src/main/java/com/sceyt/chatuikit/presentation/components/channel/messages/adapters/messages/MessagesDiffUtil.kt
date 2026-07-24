13d51dc717a9f00cdbb8e81166d2e622df05e0a2
maratsct <marat@sceyt.com>
2026-07-24 18:09:59 +0400

    Refactor `MessagesDiffUtil` to simplify item comparison logic
    - Simplified `areItemsTheSame` by replacing manual type-specific identity checks with a direct equality check between items.
    - Updated `getChangePayload` return type to non-nullable `Any` and changed the default return value from `null` to `Unit`.

--- SceytChatUiKit/src/main/java/com/sceyt/chatuikit/presentation/components/channel/messages/adapters/messages/MessagesDiffUtil.kt
+++ SceytChatUiKit/src/main/java/com/sceyt/chatuikit/presentation/components/channel/messages/adapters/messages/MessagesDiffUtil.kt
@@ -19,21 +19,7 @@ class MessagesDiffUtil(
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
         val oldItem = oldList[oldItemPosition]
         val newItem = newList[newItemPosition]
-        return when {
-            oldItem is MessageListItem.MessageItem && newItem is MessageListItem.MessageItem ->
-                oldItem.message.tid == newItem.message.tid
-
-            oldItem is MessageListItem.DateSeparatorItem && newItem is MessageListItem.DateSeparatorItem ->
-                oldItem.messageTid == newItem.messageTid
-
-            oldItem is MessageListItem.UnreadMessagesSeparatorItem &&
-                    newItem is MessageListItem.UnreadMessagesSeparatorItem ->
-                oldItem.msgId == newItem.msgId
-
-            oldItem is MessageListItem.LoadingPrevItem && newItem is MessageListItem.LoadingPrevItem -> true
-            oldItem is MessageListItem.LoadingNextItem && newItem is MessageListItem.LoadingNextItem -> true
-            else -> false
-        }
+        return (oldItem == newItem)
     }
 
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
@@ -44,11 +30,11 @@ class MessagesDiffUtil(
         else oldItem == newItem
     }
 
-    override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int): Any? {
+    override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int): Any {
         val oldItem = oldList[oldItemPosition]
         val newItem = newList[newItemPosition]
         if (oldItem is MessageListItem.MessageItem && newItem is MessageListItem.MessageItem)
             return oldItem.message.diff(newItem.message)
-        return null
+        return Unit
     }
 }
