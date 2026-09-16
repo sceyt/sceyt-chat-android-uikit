70096a2ac7564e6aa8389722d101197c6b958187
maratsct <marat@sceyt.com>
2026-09-16 17:18:28 +0400

    feat: add support for message pinning events and update SDK versions - Version "2.1.523025-SNAPSHOT"

--- buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
+++ buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
@@ -11,8 +11,8 @@ object Config {
     const val mavenCentralArtifactId = "sceyt-chat-android-uikit"
 
     // const val mavenCentralVersion = "2.1.5"
-     const val mavenCentralVersion = "local"
-    //const val mavenCentralVersion = "2.1.523024-SNAPSHOT"
+    // const val mavenCentralVersion = "local"
+    const val mavenCentralVersion = "2.1.523025-SNAPSHOT"
 
     /** App version */
     const val versionCode = 62
