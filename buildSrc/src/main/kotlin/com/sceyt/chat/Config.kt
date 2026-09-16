4d75e3c7f46ab3448a752f7575bc3241993b5fca
maratsct <marat@sceyt.com>
2026-09-16 19:43:23 +0400

    Version 2.1.52302501-SNAPSHOT

--- buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
+++ buildSrc/src/main/kotlin/com/sceyt/chat/Config.kt
@@ -15,7 +15,7 @@ object Config {
 
         // const val version = "2.1.5"
         // const val version = "local"
-        const val version = "2.1.523025-SNAPSHOT"
+        const val version = "2.1.52302501-SNAPSHOT"
     }
 
     object ChatConnection {
